245d170811ca7d14c01d89877d86b687291b66fb
Alireza Alidoust <a.alidoost@gmail.com>
2019-05-26 23:47:45 +0200

    remove dist form git

--- dist/routes/trailer.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
-const controllers_1 = require("../controllers");
-const routerHelper_1 = require("../common/routerHelper");
-const router = express.Router();
-router.post("/", routerHelper_1.awaitHandlerFactory(controllers_1.TrailerController.getTrailerByViaPlayURL()));
-exports.default = router;
